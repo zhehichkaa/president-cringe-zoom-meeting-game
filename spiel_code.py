56cf064dbba35ff52aae32d0ae2470c3baa4e02c
zhehichka <stud38@redi-school.org>
2026-04-23 18:56:33 +0200

    Buttons als Classes

--- spiel_code.py
+++ spiel_code.py
@@ -8,38 +8,31 @@ pygame.font.init()
 
 #text_surface = text_font.render('text', False, (225, 225, 225))
 
+#----------------------------classen---------------------------------------
+class Button:
+    def __init__(self, filename, width, height, x, y):
+        self.image = pygame.image.load(filename)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+    
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.topleft))
+        
 #-------SCREEN--------------------------------------------------
 screen_width = 1360
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 #................... TASTEN .................................................
-
-#-------LEAVE MEETING TASTE------------------------------------
-button_width = 200
-button_height = 50
-button_image = pygame.image.load("Leave Meeting Taste.png")
-button_image = pygame.transform.scale(button_image, (button_width, button_height))
-button_position = (screen_width - button_width, screen_height - button_height)
-button_rect = button_image.get_rect()
-button_rect.topleft = button_position
-#--------------------------------------------------------------
-
-#-------CHAT------------------------------------------
-Chat_width = 150
-Chat_height = 50
-Chat_image = pygame.image.load("Chat.png")
-Chat_image = pygame.transform.scale(Chat_image, (Chat_width, Chat_height))
-Chat_position = (450, 750)    #(screen_width - Chat_width, screen_height - Chat_height)
-Chat_rect = Chat_image.get_rect()
-Chat_rect.topleft = Chat_position
+leave_button = Button("Leave Meeting Taste.png", 200, 50, screen_width - 200, screen_height - 50)
+chat_button =  Button("Chat.png", 150, 50, 450, 750) 
 
 chatpanel_width = 500
 chatpanel_height = 700
 chatpanel = pygame.image.load("chatpanel.png")
 chatpanel = pygame.transform.scale(chatpanel, (chatpanel_width, chatpanel_height))
 chatpanel_position = (200, 70)
-#--------------------------------------------------------------
 #......................................................................
 
 clock = pygame.time.Clock()
@@ -54,10 +47,9 @@ while game_on:
         if event.type == pygame.QUIT:
             game_on = False
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            if button_rect.collidepoint(event.pos):
+            if leave_button.rect.collidepoint(event.pos):
                 game_on = False
-            if Chat_rect.collidepoint(event.pos):
-                print (show_chat_panel)
+            if chat_button.rect.collidepoint(event.pos):
                 show_chat_panel = not show_chat_panel
                 
 #.............. TASTEN EINZEIGEN .......................
@@ -65,8 +57,8 @@ while game_on:
                 
     if (show_chat_panel):
         screen.blit(chatpanel, (chatpanel_position))
-    screen.blit(Chat_image, (Chat_position))
-    screen.blit(button_image, (button_position))
+    chat_button.draw(screen)
+    leave_button.draw(screen)
     
     pygame.display.flip()
     
